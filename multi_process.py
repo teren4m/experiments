dacfa16f273438d2e2fbd79e8421faef97e62d44
Tim Andriyaschenko <timAndriyaschenko@gmail.com>
2023-01-27 23:01:40 +0200

    Update process

--- multi_process.py
+++ multi_process.py
@@ -1,25 +1,33 @@
-def do_math_homework():
 
+from multiprocessing import Process
+
+def do_math_homework():
     res = 0
     for i in range(100_000_000):
         res += i
     return res
 
-def do_physics_homework(num_of_tasks:int):
-  """same as do_math_homework)"""
-def do_chemistry_homework(num_of_tasks:int):
-  """same as do_math_homework)"""
+def do_physics_homework():
+    res = 0
+    for i in range(100_000_000):
+        res += i
+    return res
 
-from multiprocessing import Process
+def do_chemistry_homework():
+    res = 0
+    for i in range(100_000_000):
+        res += i
+    return res
 
 processlist = []
 
-processlist.append(Process(target=do_math_homework))
-processlist.append(Process(target=do_physics_homework))
-processlist.append(Process(target=do_chemistry_homework))
-
-for t in processlist:
-  t.start()
-
-for t in processlist:
-  t.join()
+if __name__ == '__main__':
+    processlist.append(Process(target=do_math_homework))
+    processlist.append(Process(target=do_physics_homework))
+    processlist.append(Process(target=do_chemistry_homework))
+    
+    for t in processlist:
+        t.start()
+        
+    for t in processlist:
+        t.join()
